8357c476630e7a970c6e018361272afe8a351fe7
saucersauros <733434@student.dusd.net>
2026-02-03 15:30:52 -0800

    das

--- P7PlayerControlSandoval/Assets/Scripts/FollowPlayer.cs
+++ P7PlayerControlSandoval/Assets/Scripts/FollowPlayer.cs
@@ -2,12 +2,26 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    private Vector3 offset = new Vector3(0, 5, -7);
+    public Vector3 offset = new Vector3(0, 5, -7);
     public GameObject player;
+    public GameObject camera;
    
     // Update is called once per frame
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
     }
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            camera.Set.Active(true);
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            offset = new Vector3(0, 5, -7);
+        }
+    }
+
 }
--- P7PlayerControlSandoval/Assets/Scripts/PlayerController.cs
+++ P7PlayerControlSandoval/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -6,6 +8,11 @@ public class PlayerController : MonoBehaviour
     public int speed = 20;
     public int turnspeed;
     public float horizontalInput;
+    public float forwardInput;
+    public int nitro;
+    public int afterspeed;
+    public GameObject particle;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,9 +22,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        forwardInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
-}        //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        transform.Translate(Vector3.right * Time.deltaTime * turnspeed);
-    }
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnspeed * horizontalInput);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            speed = nitro;
+            particle.SetActive(true);
+
+        }
+        else
+        {
+            speed = afterspeed;
+            particle.SetActive(false);
+        }
+    }        //Move the vehicle forward
+    
 }

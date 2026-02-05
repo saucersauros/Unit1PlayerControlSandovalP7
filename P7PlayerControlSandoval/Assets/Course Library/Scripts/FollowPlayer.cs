9f917b7d7a94088692a49d7c784873c82b9bf115
saucersauros <733434@student.dusd.net>
2026-02-05 15:25:40 -0800

    fixed some stuff

--- P7PlayerControlSandoval/Assets/Course Library/Scripts/FollowPlayer.cs
+++ P7PlayerControlSandoval/Assets/Course Library/Scripts/FollowPlayer.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 
-public class FollowPlayer : MonoBehaviour
+public class FollowPlayer1 : MonoBehaviour
 {
     public Vector3 offset = new Vector3(0, 5, -7);
     public GameObject player;
     public GameObject camera;
-
+   
     // Update is called once per frame
     void LateUpdate()
     {
@@ -16,7 +16,7 @@ public class FollowPlayer : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.K))
         {
-            offset = new Vector3(0, 4, 8);
+            offset = new Vector3(0, 3, 10);
         }
         if (Input.GetKeyDown(KeyCode.L))
         {

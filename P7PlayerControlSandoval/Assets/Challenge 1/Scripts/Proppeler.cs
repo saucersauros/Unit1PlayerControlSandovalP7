f11e1b313731b42fbbd22afe76fa192427489970
saucersauros <733434@student.dusd.net>
2026-06-03 12:29:01 -0700

    asf
    asd

--- P7PlayerControlSandoval/Assets/Challenge 1/Scripts/Proppeler.cs
+++ P7PlayerControlSandoval/Assets/Challenge 1/Scripts/Proppeler.cs
@@ -6,18 +6,10 @@ public class Proppeler : MonoBehaviour
     public float rotationSpeed;
     public float verticalInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
-
         transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * 5);
     }
 }
